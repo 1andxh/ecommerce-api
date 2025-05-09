f2b34539628b4c350a79f2aa321282576a3df70f
1andxh <solomonkingsman@gmail.com>
2025-05-09 17:55:21 +0000

    create order models

--- orders/models.py
+++ orders/models.py
@@ -16,16 +16,28 @@ class Order(models.Model):
         CANCELLED = 'Cancelled'
 
     order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    order_number = models.CharField
+    order_number = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.Choices(choices=StatusChoices.choices, default=StatusChoices.PENDING)
-    products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through="OrderItem", related_name='orders')
 
 
     def __str__(self) -> str:
         return f"Order placed for {self.user}\nOrder: {self.order_id}"
     
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    @property
+    def item_total(self):
+        return self.product.price * self.quantity
+    
+    def __str__(self) -> str:
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
 
 
 
