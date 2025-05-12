d24100c493560b66c040f42259a90c87348b6b12
1andxh <solomonkingsman@gmail.com>
2025-05-12 18:49:02 +0000

    populate db

--- management/commands/populate_db.py
+++ management/commands/populate_db.py
@@ -1,4 +1,7 @@
 from django.core.management.base import BaseCommand
+from user.models import User
+from products.models import Product
+from orders.models import Order, OrderItem 
 
 
 class Command(BaseCommand):
@@ -6,3 +9,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **option):
         self.stdout.write('Running custom commands...')
+        
--- products/views.py
+++ products/views.py
@@ -1,8 +1,16 @@
 # from django.shortcuts import render
 from rest_framework import viewsets, status
-from rest_framework import Response
+from rest_framework import response
+from django.http import JsonResponse
 from products.models import Product
 from products.serializers import ProductSerializer
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
+# class ProductViewSet(viewsets.ModelViewSet):
+#     queryset = Product.objects.all()
+
+def product_list():
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse({
+        'data' : serializer.data
+    })
